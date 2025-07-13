c72ed906bb12230fcc73e7fc50691197937e49df
jeeva <jeeva.v6499@gmail.com>
2025-07-13 11:45:34 +0530

    footer added commit no 11

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,34 @@
-
+import { FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-    footer 
+      <section className="bg-[#121418]  footer-center text-white p-14">
+        <div className="w-full max-w-screen-xl mx-auto px-4 flex flex-col items-center font-inter  ">
+          <div className="quickLinks flex gap-5 my-5 text-sm font-light">
+            <span>Terms Of Use </span>
+            <span>Privacy Policy</span>
+            <span>About</span>
+            <span> Blog</span>
+            <span>FAQ</span>
+          </div>
+          <div className="socialMedia flex gap-5  my-5">
+            <span className=" inline-block">
+              <FaTwitter className="text-xl" />
+            </span>
+            <a href="https://github.com/jeeva-2708">
+              <span className="inline-block">
+                <FaGithub className="text-xl " />
+              </span>
+            </a>
+          </div>
+          <div className="copyrights text-base italic my-5 font-light">
+            <p>© 2025 Movimaniax. All rights reserved. Developed by Jeeva.</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
