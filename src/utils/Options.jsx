c2eda590da87ba33d218b7c48863f4d5490af5d1
jeeva <jeeva.v6499@gmail.com>
2025-08-01 10:51:37 +0530

    Remove exposed token

--- src/utils/Options.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Y2E5YzM1OTI2ODA2M2MyMmFhZTNmODg5YTdhZTM2NSIsIm5iZiI6MTc1MDgzNTU5Ny4yMywic3ViIjoiNjg1YmExOGQzYWQ1ZmEyNmMxOTFiM2NmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.3ojUU7hD7WTK8-iF03U7JZVEgLv53ehzcklC9i7bIIU'
-  }
-};
